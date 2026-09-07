48be15547be50e9ee0b055825238baf8d5e8438d
MultiDeal <prosebmail@gmail.com>
2026-09-07 06:58:43 -0400

    Update sitemap URL in robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: ['/api/'],
     },
-    sitemap: 'https://votredomaine.com/sitemap.xml',
+    sitemap: 'https://multidealprop.com/sitemap.xml',
   };
 }
